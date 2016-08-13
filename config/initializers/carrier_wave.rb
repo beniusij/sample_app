a56c62411bc946dd6e1d8cb952fe39e1e157c811
beniusij <beniusij@gmail.com>
2016-08-13 21:08:27 +0300

    Add user microposts

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,5 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.root = Rails.root.join('tmp')
-    config.cache_dir = 'carrierwave'
-
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider               => 'AWS',
