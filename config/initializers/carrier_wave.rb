39924bc01d3fc6e0faf7d659ae12fce90095bee2
beniusij <beniusij@gmail.com>
2016-08-13 14:41:40 +0300

    Edited carrierwave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,19 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.root = Rails.root.join('tmp')
+    config.cache_dir = 'carrierwave'
+
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AKIAJ3C7QZWYOAKCOAHQ'],
-      :aws_secret_access_key  => ENV['mNSI2rZHx6Zc9y6pdjP/k+8AvJTMFUHGowagcJb']
+      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
+      :region                 => 'eu-west-1',
+      :host                   => 's3.example.com',
+      :endpoint               => 'https://s3.example.com:8000'
     }
-    config.fog_directory      =  ENV['juozasbucket']
+    config.fog_directory      = ENV['S3_Bucket']
+    config.fog_public         = false
+    config.fog_attributes     = {'Cache-Control'=>'max-age=31576000'}
   end
 end
